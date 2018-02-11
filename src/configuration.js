2e913d73e3eff1c4432d27dcc84b213e981b8f67
Scott Talbot <s@chikachow.org>
2018-02-11 21:34:45 +1100

    Flesh out configuration type

--- src/configuration.js
+++ src/configuration.js
@@ -1,8 +1,36 @@
 const configuration = {
+  features: {
+    featureOne: {
+      '.conditional': (target) => {
+        if (target.platform.platform == 'iOS') {
+          return true;
+        }
+        return false;
+      },
+    },
+  },
 };
 
 
 const resolve = (obj, target) => {
+  if (obj == null) {
+    return obj;
+  }
+  if (Array.isArray(obj)) {
+    return obj.map((o) => {
+      return resolve(o, target);
+    });
+  }
+  if (typeof obj === "object" && obj['.conditional']) {
+    obj = resolve(obj['.conditional'](target));
+  }
+  if (typeof obj === "object" && obj !== null) {
+    var clone = {};
+    for (const property in obj) {
+      clone[property] = resolve(obj[property], target);
+    }
+    obj = clone
+  }
   return obj;
 };
 

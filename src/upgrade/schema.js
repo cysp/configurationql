68737cf90eef599c58388a9368fc5d614f63a55d
Scott Talbot <s@chikachow.org>
2018-02-23 23:27:35 +1100

    .

--- src/upgrade/schema.js
+++ src/upgrade/schema.js
@@ -1,5 +1,46 @@
+const {
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLUnionType,
+  GraphQLSchema,
+  GraphQLString,
+} = require('graphql');
+
+const {
+  TargetType,
+} = require('../types');
+
+const {
+  UpgradeActionType,
+  UpgradeDetailType,
+  SuggestedUpgradeDetailType,
+  MandatoryUpgradeDetailType,
+} = require('./types');
+
+
 module.exports = {
   query: {
-    upgrade: require('./schema/query/upgrade'),
+    upgrade: {
+      type: UpgradeDetailType,
+      args: {
+          target: { type: new GraphQLNonNull(TargetType) },
+      },
+      resolve: (parent, { target }, ctx, info) => {
+          if (target.platform.platform == 'iOS') {
+              if (target.version < '1.0.0') {
+                  return {
+                      action: 'FORCE',
+                      reason: 'We turned old stuff off.',
+                  };
+              }
+              if (target.version < '2.0.0') {
+                  return {
+                      action: 'SUGGEST',
+                  };
+              }
+          }
+          return null;
+      }
+    },
   },
 };
